cea94c4c81c6a0343bd5eaedcadbe1156a450ce4
Kylepoops <62238380+Kylepoops@users.noreply.github.com>
2021-11-25 22:53:30 +0800

    implement basic result notification

--- src/main/kotlin/io/craftray/huntrace/game/GameResultExtend.kt
+++ src/main/kotlin/io/craftray/huntrace/game/GameResultExtend.kt
@@ -1,5 +1,7 @@
 package io.craftray.huntrace.game
 
+import net.kyori.adventure.text.Component
+
 enum class GameResult {
     HUNTER_WIN,
     SURVIVOR_WIN,
@@ -14,10 +16,22 @@ fun Game.matchResult(result: GameResult) = when (result) {
     GameResult.HUNTER_QUIT -> this.hunterQuit()
 }
 
-fun Game.hunterWin() {}
+fun Game.hunterWin() {
+    this.hunters.forEach { it.sendMessage(Component.text("hunter win")) }
+    this.survivor.sendMessage(Component.text("hunter win"))
+}
 
-fun Game.survivorWin() {}
+fun Game.survivorWin() {
+    this.hunters.forEach { it.sendMessage(Component.text("survivor win")) }
+    this.survivor.sendMessage(Component.text("survivor win"))
+}
 
-fun Game.survivorQuit() {}
+fun Game.survivorQuit() {
+    this.hunters.forEach { it.sendMessage(Component.text("survivor quit")) }
+    this.survivor.sendMessage(Component.text("survivor quit"))
+}
 
-fun Game.hunterQuit() {}
+fun Game.hunterQuit() {
+    this.hunters.forEach { it.sendMessage(Component.text("hunter quit")) }
+    this.survivor.sendMessage(Component.text("hunter quit"))
+}
