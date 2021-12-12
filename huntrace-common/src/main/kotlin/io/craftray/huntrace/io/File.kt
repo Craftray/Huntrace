939313f53b942abc69b2540440b3546638f05ebb
Kylepoops <62238380+Kylepoops@users.noreply.github.com>
2021-12-13 07:44:20 +0800

    add some comments to the obscure code

--- huntrace-common/src/main/kotlin/io/craftray/huntrace/io/File.kt
+++ huntrace-common/src/main/kotlin/io/craftray/huntrace/io/File.kt
@@ -6,17 +6,31 @@ import java.util.concurrent.Future
 
 val executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())!!
 
+/**
+ * Delete the directory and all its contents asynchronously
+ * @param await whether to wait for the deletion to complete
+ * @param futures if you need to wait for the deletion to complete, pass in a set here and use Set#forEach(Future<*>::get)
+ */
 fun File.deepDeleteAsync(await: Boolean = false, futures: MutableSet<Future<*>> = mutableSetOf()) {
+    // first submit the task and get the future
     val future = executor.submit {
         if (this.exists()) {
             if (this.isDirectory) {
+                // Construct another future set here
+                // Because we need all the subdirectories and files to be deleted before this directory is deleted
                 val thisFutures = mutableSetOf<Future<*>>()
+                // Pass the future set to this, so we can store all the future
                 this.listFiles()?.forEach { it.deepDeleteAsync(futures = thisFutures) }
+                // Wait for all the subdirectories and files to be deleted
                 thisFutures.forEach(Future<*>::get)
             }
+            // Finally, delete this file or directory
             this.delete()
         }
     }
+    // Add the future to the future set
     futures.add(future)
+    // Wait the task to finish before returning if await is true
+    // It shouldn't be called inside this function
     if (await) future.get()
 }
