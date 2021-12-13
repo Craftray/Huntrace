0963400268ea0cb9c04b475da52308ff72fa547f
Kylepoops <62238380+Kylepoops@users.noreply.github.com>
2021-12-13 09:20:12 +0800

    make executor private

--- huntrace-common/src/main/kotlin/io/craftray/huntrace/io/File.kt
+++ huntrace-common/src/main/kotlin/io/craftray/huntrace/io/File.kt
@@ -4,7 +4,7 @@ import java.io.File
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
-val executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())!!
+private val executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())!!
 
 /**
  * Delete the directory and all its contents asynchronously.<br/>
